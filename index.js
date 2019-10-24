114fb33441af5782888325b1db73be100cdfa2ab
Mark Barr <mark@hyperingenuity.com>
2019-10-24 13:20:30 -0400

    mesh: circular seen split out into own module, test added

--- index.js
+++ index.js
@@ -4,39 +4,7 @@ const tls = require('tls');
 const async = require('async');
 const crypto = require('crypto');
 const selfsigned = require('selfsigned');
-
-function CircularSeen(capacity = 100) {
-  const seen = new Array(capacity);
-  const set = new Set();
-
-  let start = 0;
-  let size = 0;
-
-  this.size = () => { return size; };
-
-  this.add = (id) => {
-    if (start > 0) {
-      start--;
-    } else {
-      start = capacity - 1;
-    }
-
-    if (seen[start]) {
-      set.delete(seen[start]);
-    }
-
-    seen[start] = id;
-    set.add(id);
-
-    if (size < capacity) {
-      size++;
-    }
-  };
-
-  this.has = (id) => {
-    return set.has(id);
-  };
-}
+const CircularSeen = require('./circularSeen');
 
 function Mesh(skyfall, options) {
   this.id = skyfall.utils.id();
---
+++ seen.test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const uuid = require('uuid/v4');
+const CircularSeen = require('./circularSeen');
+
+const CAPACITY = 10;
+
+describe('Skyfall Mesh Networking Circular Seen Test', () => {
+  let seen;
+  const initial = uuid();
+
+  it(`shoud create a new circular seen object with capacity ${ CAPACITY }`, () => {
+    seen = new CircularSeen(CAPACITY);
+  });
+
+  it('should add and verify the presence of one id', () => {
+    seen.add(initial);
+    expect(seen.has(initial)).toBe(true);
+  });
+
+  it(`should add ${ CAPACITY } ids and verify the initial id is not present`, () => {
+    for (let i = 0; i < CAPACITY; i++) {
+      seen.add(uuid());
+    }
+
+    expect(seen.has(initial)).toBe(false);
+  });
+});
